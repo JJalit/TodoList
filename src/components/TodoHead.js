b624ca2f33e4e2b890a83d134f1b6b0ee195a5ab
jaeha <jaeha23@naver.com>
2021-10-15 11:46:28 +0900

    check TodoHead

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import { useTodoState } from "../TodoContext";
+
 const TodoHeadBlock = styled.div`
   padding: 48px 32px 24px;
   border-bottom: 1px solid #e9ecef;
@@ -23,6 +25,9 @@ const TodoHeadBlock = styled.div`
 `;
 
 function TodoHead() {
+  const todos = useTodoState();
+  console.log(todos);
+
   return (
     <TodoHeadBlock>
       <h1>2019년 7월 10일</h1>
